8d7be8f87f70aca6b2a98b864a2fc31826ad4492
Nils HOLZENBERGER <Nils@MacBook-Pro-de-Nils.local>
2016-02-07 11:54:56 -0500

    code restructuration

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
-ocamlopt -o setosa_vs_virginica LinearAlgebra.ml AdvancedPerceptron.ml VonNeumann.ml PalRujanPerceptron.ml SetosaVsVirginica.ml 
-ocamlopt -o setosa_vs_versicolor LinearAlgebra.ml AdvancedPerceptron.ml VonNeumann.ml PalRujanPerceptron.ml SetosaVsVersicolor.ml 
-ocamlopt -o virginica_vs_versicolor LinearAlgebra.ml AdvancedPerceptron.ml VonNeumann.ml PalRujanPerceptron.ml VirginicaVsVersicolor.ml 
+mkdir egs
+cd src/
+ocamlopt -o ../egs/setosa_vs_virginica LinearAlgebra.ml AdvancedPerceptron.ml VonNeumann.ml PalRujanPerceptron.ml SetosaVsVirginica.ml 
+ocamlopt -o ../egs/setosa_vs_versicolor LinearAlgebra.ml AdvancedPerceptron.ml VonNeumann.ml PalRujanPerceptron.ml SetosaVsVersicolor.ml 
+ocamlopt -o ../egs/virginica_vs_versicolor LinearAlgebra.ml AdvancedPerceptron.ml VonNeumann.ml PalRujanPerceptron.ml VirginicaVsVersicolor.ml 
 
